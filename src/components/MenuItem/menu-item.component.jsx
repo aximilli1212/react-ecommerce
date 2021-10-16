e80a85db48f110bbb8fafe2154438cf611a85374
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-16 10:38:38 +0000

    add gow on hover to each item.

--- src/components/MenuItem/menu-item.component.jsx
+++ src/components/MenuItem/menu-item.component.jsx
@@ -5,13 +5,12 @@ const MenuItem = ({ title, imageUrl, size }) => (
   <div className={`menu-item ${size}`}>
     <div
       style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`menu-item ${size}`}
-    >
+      className={`background-image ${size}`}
+     />
       <div className="content">
         <div className="title">{title}</div>
         <span className="subtitle">SHOP NOW</span>
       </div>
-    </div>
   </div>
 );
 
