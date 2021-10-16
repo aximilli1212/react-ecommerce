efe928033fe912854ccf9dfbafe03e522079168e
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-16 12:00:23 +0000

    destructure and spread props inside of directory map iteration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react'
+import {Switch, Route} from "react-router-dom";
 import Homepage from "./Homepage/homepage.component";
 import './App.css';
+
+const HatsPage = ()=>(
+    <div>
+        <h1>
+            HATS PAGE
+        </h1>
+    </div>
+)
 
 function App() {
   return (
     <div className="App">
      <Homepage/>
+        <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
