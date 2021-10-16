0706d93a6871f3cf5cecf444865f5684ef963635
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-16 06:40:13 +0000

    move background image up one level in MenuItem

--- src/components/MenuItem/menu-item.component.jsx
+++ src/components/MenuItem/menu-item.component.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './menu-item.styles.scss'
+import React from "react";
+import "./menu-item.styles.scss";
 
-const MenuItem = ({title})=>(
-    <div className="menu-item">
-        <div className="content">
-            <div className="title">{title}</div>
-            <span className="subtitle">Subtitle</span>
-        </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`menu-item ${size}`}>
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className={`menu-item ${size}`}
+    >
+      <div className="content">
+        <div className="title">{title}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-)
+  </div>
+);
 
 export default MenuItem;
