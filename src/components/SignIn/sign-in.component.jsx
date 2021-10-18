aa55f74e951db1eddd8b04b90cff121e06d42094
Emmanuel Sackey <aximilli1212@gmail.com>
2021-10-18 10:19:31 +0000

    Add components set for signin

--- src/components/SignIn/sign-in.component.jsx
+++ src/components/SignIn/sign-in.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import FormInput from "../FormInput/form-input.component";
 
 class SignIn extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            email: '',
+            email: 'd',
             password:''
+        }
+
+        const handleChange = ()=>{
+
         }
     }
 
@@ -15,6 +20,22 @@ class SignIn extends React.Component {
             <div className='sign-in'>
                 <h2> I already have an account</h2>
                 <span>Sign in with your email and password.</span>
+                <FormInput
+                    name='email'
+                    type='email'
+                    label='Email'
+                    value={this.state.email}
+                    handleChange={this.handleChange}
+                    required
+                />
+                <FormInput
+                 name='password'
+                 type='password'
+                 label='Password'
+                 value={this.state.password}
+                 handleChange={this.handleChange}
+                 required
+                />
             </div>
         )
     }
